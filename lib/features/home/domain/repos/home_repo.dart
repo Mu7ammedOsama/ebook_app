6afb8264b0e9aaa67ad18b7814c63b6c3dcf52cc
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2023-12-31 22:14:34 +0200

    Add Either to Home Repo

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:ebook_app/core/errors/failures.dart';
 import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<Either<Failures, List<BookEntity>>> fetchFeaturedBooks();
 
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failures, List<BookEntity>>> fetchNewestBooks();
 }
