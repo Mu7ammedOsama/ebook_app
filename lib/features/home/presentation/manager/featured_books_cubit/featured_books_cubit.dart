f8c09d690fdfd473dd1ae2b6217a7ec1711d4947
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2024-01-01 18:08:42 +0200

    Create Newest Books Cubits

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
 import 'package:meta/meta.dart';
 
-part 'featured_book_state.dart';
+part 'featured_books_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
   FeaturedBookCubit() : super(FeaturedBookInitial());
