c8b20ae335b770c9ce068e91de2956a958764b89
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2024-01-01 15:37:55 +0200

    Create Server Failures from Dio Error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -3,46 +3,43 @@ import 'package:dio/dio.dart';
 abstract class Failures {
   final String errorMessage;
 
-  const Failures(this.errorMessage);
+  Failures(this.errorMessage);
 }
 
-// class ServerFailures extends Failures {
-//   ServerFailures(super.errorMessage);
-//
-//   factory ServerFailures.fromDioError(DioError dioError) {
-//     switch (dioError.type) {
-//       case DioExceptionType.connectionTimeout:
-//         return ServerFailures('Connection Time Out with API Service');
-//       case DioExceptionType.sendTimeout:
-//         return ServerFailures('Send Time Out with API Service');
-//       case DioExceptionType.receiveTimeout:
-//         return ServerFailures('Receive Time Out with API Service');
-//       case DioExceptionType.badResponse:
-//         return ServerFailures.fromResponse(
-//           dioError.response!.statusCode,
-//           dioError.response!.data,
-//         );
-//       case DioExceptionType.cancel:
-//         return ServerFailures('Request with API Service was Canceled');
-//       case DioExceptionType.unknown:
-//         if (dioError.message!.contains('SocketException')) {
-//           return ServerFailures('No Internet Connection');
-//         }
-//         return ServerFailures('Unexpected Error, Please try later!');
-//       default:
-//         return ServerFailures('Opps there was an error, Please try again!');
-//     }
-//   }
-//
-//   factory ServerFailures.fromResponse(int? statusCode, dynamic response) {
-//     if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
-//       return ServerFailures(response['error']['message']);
-//     } else if (statusCode == 404) {
-//       return ServerFailures('Your request not found, Please try later!');
-//     } else if (statusCode == 500) {
-//       return ServerFailures('Internal Server Error, Please try later!');
-//     } else {
-//       return ServerFailures('Opps there was an error, Please try again!');
-//     }
-//   }
-// }
+class ServerFailures extends Failures {
+  ServerFailures(super.errorMessage);
+
+  factory ServerFailures.fromDiorError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailures('Connection Time Out with API Service');
+      case DioExceptionType.sendTimeout:
+        return ServerFailures('Send Time Out with API Service');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailures('Receive Time Out with API Service');
+      case DioExceptionType.badResponse:
+        return ServerFailures.fromResponse(dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailures('Request with API Service was Canceled');
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailures('No Internet Connection');
+        }
+        return ServerFailures('Unexpected Error, Please try later!');
+      default:
+        return ServerFailures('Opps There was an Error, Please try again');
+    }
+  }
+
+  factory ServerFailures.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailures('Your request not found, Please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailures('Internal Server Error, Please try later!');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailures(response['error']['message']);
+    } else {
+      return ServerFailures('Opps there was an error, Please try again!');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,74 +1,5 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:dio/dio.dart';
-// import 'package:ebook_app/core/errors/failures.dart';
-// import 'package:ebook_app/core/utilities/api_service.dart';
-// import 'package:ebook_app/features/home/data/models/book_model/book_model.dart';
-// import 'package:ebook_app/features/home/domain/repos/home_repo.dart';
-//
-// class HomeRepoImplementation implements HomeRepo {
-//   final APIService apiService;
-//
-//   HomeRepoImplementation(this.apiService);
-//
-//   @override
-//   Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() async {
-//     try {
-//       var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
-//
-//       List<BookModel> books = [];
-//
-//       for (var item in data['items']) {
-//         books.add(BookModel.fromJson(item));
-//       }
-//       return right(books);
-//     } catch (e) {
-//       if (e is DioException) {
-//         return left(ServerFailures.fromDioError(e));
-//       }
-//       return left(ServerFailures(e.toString()));
-//     }
-//   }
-//
-//   @override
-//   Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
-//     try {
-//       var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=subject=Computer Science');
-//
-//       List<BookModel> books = [];
-//
-//       for (var item in data['items']) {
-//         books.add(BookModel.fromJson(item));
-//       }
-//       return right(books);
-//     } catch (e) {
-//       if (e is DioException) {
-//         return left(ServerFailures.fromDioError(e));
-//       }
-//       return left(ServerFailures(e.toString()));
-//     }
-//   }
-//
-//   @override
-//   Future<Either<Failures, List<BookModel>>> fetchSimilarBooks({required String category}) async {
-//     try {
-//       var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=relevance&q=subject=Computer Science');
-//
-//       List<BookModel> books = [];
-//
-//       for (var item in data['items']) {
-//         books.add(BookModel.fromJson(item));
-//       }
-//       return right(books);
-//     } catch (e) {
-//       if (e is DioException) {
-//         return left(ServerFailures.fromDioError(e));
-//       }
-//       return left(ServerFailures(e.toString()));
-//     }
-//   }
-// }
-
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:ebook_app/core/errors/failures.dart';
 import 'package:ebook_app/features/home/data/data_sources/home_local_data_source.dart';
 import 'package:ebook_app/features/home/data/data_sources/home_remote_data_source.dart';
@@ -79,10 +10,7 @@ class HomeRepoImplementation extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
   final HomeLocalDataSource homeLocalDataSource;
 
-  HomeRepoImplementation({
-    required this.homeRemoteDataSource,
-    required this.homeLocalDataSource,
-  });
+  HomeRepoImplementation({required this.homeRemoteDataSource, required this.homeLocalDataSource});
 
   @override
   Future<Either<Failures, List<BookEntity>>> fetchFeaturedBooks() async {
@@ -98,7 +26,10 @@ class HomeRepoImplementation extends HomeRepo {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failures());
+      if (e is DioException) {
+        return left(ServerFailures.fromDiorError(e));
+      }
+      return left(ServerFailures(e.toString()));
     }
   }
 
@@ -116,7 +47,10 @@ class HomeRepoImplementation extends HomeRepo {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failures());
+      if (e is DioException) {
+        return left(ServerFailures.fromDiorError(e));
+      }
+      return left(ServerFailures(e.toString()));
     }
   }
 }
