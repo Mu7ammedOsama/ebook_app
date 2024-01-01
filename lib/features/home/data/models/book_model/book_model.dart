93aac8cc93787c2fc6eb8d51335f284de22aaddb
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2024-01-01 10:46:42 +0200

    Create Book Model Extends Book Entity

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,36 +1,43 @@
-import 'package:equatable/equatable.dart';
+import 'package:ebook_app/features/home/domain/entities/book_entity.dart';
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel extends Equatable {
-  final String? kind;
-  final String? id;
-  final String? etag;
-  final String? selfLink;
-  final VolumeInfo volumeInfo;
-  final SaleInfo? saleInfo;
-  final AccessInfo? accessInfo;
-  final SearchInfo? searchInfo;
+class BookModel extends BookEntity {
+  String? kind;
+  String? id;
+  String? etag;
+  String? selfLink;
+  VolumeInfo? volumeInfo;
+  SaleInfo? saleInfo;
+  AccessInfo? accessInfo;
+  SearchInfo? searchInfo;
 
-  const BookModel({
+  BookModel({
     this.kind,
     this.id,
     this.etag,
     this.selfLink,
-    required this.volumeInfo,
+    this.volumeInfo,
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+          bookId: id!,
+          image: volumeInfo?.imageLinks?.thumbnail ?? '',
+          title: volumeInfo!.title!,
+          author: volumeInfo.authors!.first,
+          price: 0.0,
+          rating: volumeInfo.averageRating,
+        );
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
         kind: json['kind'] as String?,
         id: json['id'] as String?,
         etag: json['etag'] as String?,
         selfLink: json['selfLink'] as String?,
-        volumeInfo: VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
+        volumeInfo: json['volumeInfo'] == null ? null : VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
         saleInfo: json['saleInfo'] == null ? null : SaleInfo.fromJson(json['saleInfo'] as Map<String, dynamic>),
         accessInfo: json['accessInfo'] == null ? null : AccessInfo.fromJson(json['accessInfo'] as Map<String, dynamic>),
         searchInfo: json['searchInfo'] == null ? null : SearchInfo.fromJson(json['searchInfo'] as Map<String, dynamic>),
@@ -41,23 +48,9 @@ class BookModel extends Equatable {
         'id': id,
         'etag': etag,
         'selfLink': selfLink,
-        'volumeInfo': volumeInfo.toJson(),
+        'volumeInfo': volumeInfo?.toJson(),
         'saleInfo': saleInfo?.toJson(),
         'accessInfo': accessInfo?.toJson(),
         'searchInfo': searchInfo?.toJson(),
       };
-
-  @override
-  List<Object?> get props {
-    return [
-      kind,
-      id,
-      etag,
-      selfLink,
-      volumeInfo,
-      saleInfo,
-      accessInfo,
-      searchInfo,
-    ];
-  }
 }
